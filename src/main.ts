e159bd027185f160f499955fc6215bf80b846b16
umidbek77 <umidbekzaripov777@gmail.com>
2026-03-16 14:22:43 +0500

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://urdu-journal.vercel.app',
+    ],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
